8423906e95f39cc2fa517df3daa67798771ebd0b
Cryptollusion <rmorgan512@protonmail.ch>
2024-04-16 11:02:00 +0100

    add consts

--- src/lib.rs
+++ src/lib.rs
@@ -5,3 +5,33 @@ pub mod standard_form;
 #[rustfmt::skip]
 #[allow(unused)]
 mod forces;
+
+const PERMEABILITY: StandardForm<f64> = StandardForm(4 * std::f64::consts::PI, -7);
+const PERMITTIVITY: StandardForm<f64> = StandardForm(8.85, -12);
+
+struct Object;
+
+struct Simulator {
+    object_pool: Vec<Object>,
+    time: u64,
+}
+
+impl Simulator {
+    fn new() -> Self {
+        Self {
+            object_pool: vec![],
+            time: 0,
+        }
+    }
+
+    fn run_indefinitely(&mut self) -> ! {
+        loop {
+            &self.simulate_tick();
+            self.time += 1
+        }
+    }
+
+    fn simulate_tick(&mut self) {
+
+    }
+}
